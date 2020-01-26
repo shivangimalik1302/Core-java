250a1cff0301c8ede0c5ed2c3b24f85fda6b7da5
Shivangi Malik <60251566+shivangimalik1302@users.noreply.github.com>
2020-01-26 22:06:40 +0530

    Add files via upload

---
+++ Searching and Sorting/SelectionSort.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class SelectionSort{
+	
+	public static int[] takeInput() {
+		Scanner s = new Scanner(System.in);
+		int size = s.nextInt();
+		int arr[] = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = s.nextInt();
+		}
+		return arr;
+	}
+
+	public static void print(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+    
+    public static void selectionSort(int[] arr){
+       for (int i = 0; i < arr.length - 1; i++)  
+        {  
+            int index = i;  
+            for (int j = i + 1; j < arr.length; j++){  
+                if (arr[j] < arr[index]){  
+                    index = j;  
+                }  
+            }  
+            int smallerNumber = arr[index];   
+            arr[index] = arr[i];  
+            arr[i] = smallerNumber;  
+        }  
+
+	}
+
+	
+	public static void main(String[] args) {
+		int[] arr = takeInput();
+		selectionSort(arr);
+		print(arr);
+	}
+    
+}
---
+++ Searching and Sorting/insertionSort.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class insertionSort {
+	
+	public static int[] takeInput() {
+		Scanner s = new Scanner(System.in);
+		int size = s.nextInt();
+		int arr[] = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = s.nextInt();
+		}
+		return arr;
+	}
+
+	public static void print(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	
+     public static void insertionSort(int[] arr){
+        for(int i=1;i<arr.length;i++)
+        {
+            for(int j=i;j>0;j--)
+            {
+                if(arr[j]<arr[j-1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
+            }
+        }
+    }
+
+
+	public static void main(String[] args) {
+		int[] arr = takeInput();
+		insertionSort(arr);
+		print(arr);
+	}
+}
